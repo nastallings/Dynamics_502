e68ee7d1728f1c055f7394f3abb39ca5a24b7440
nastallings <nstallings44@comcast.net>
2021-05-10 11:38:25 -0400

    final code

--- IK3D.m
+++ IK3D.m
@@ -1,28 +1,7 @@
 
-%% analytical method --> results in no solution for system of equations
-function [phiSol, kSol] = anIK2D(x,y,z)
-% solves for Phi and K, s is a constant  = 8.34
-s = 8.34; % for segment A
-syms phi k
-
-%M2 =  forward kinematics T matrix where right column = posVector(x,y,z)
-M2 = [cos(phi)*cos(s*k), -sin(phi), -cos(phi)*sin(s*k), (1/k)*(cos(phi) - cos(phi)*cos(s*k)); sin(phi)*cos(s*k), cos(phi), -sin(phi)*sin(s*k), -(1/k)*(sin(phi)*cos(s*k)); sin(s*k), 0, cos(s*k), (1/k)*sin(s*k);0, 0, 0, 1];
-posV = [M2(1,4) ;M2(2,4); M2(3,4)]
-
-% solve system of equations to find phi and k
-equ1 = posV(1) == x
-equ2 = posV(2) == y 
-equ3 =posV(3) == z
-
-sol = solve([equ1, equ2, equ3], [phi k]);
-phiSol = sol.phi;
-kSol = sol.k;
-
-end
-
 %% geometric method
 
-function [phi,k,theta] = IK3Dx(x, y, z)
+function [phi,k,theta] = IK3D(x, y, z)
 % section 2.2 says how to handle singularities but sam is tired
 % P  = (x,y,z) % desired point
 
@@ -45,3 +24,24 @@ function [phi,k,theta] = IK3Dx(x, y, z)
 
 end
 
+
+%% analytical method --> results in no solution for system of equations
+function [phiSol, kSol] = anIK2D(x,y,z)
+% solves for Phi and K, s is a constant  = 8.34
+s = 8.34; % for segment A
+syms phi k
+
+%M2 =  forward kinematics T matrix where right column = posVector(x,y,z)
+M2 = [cos(phi)*cos(s*k), -sin(phi), -cos(phi)*sin(s*k), (1/k)*(cos(phi) - cos(phi)*cos(s*k)); sin(phi)*cos(s*k), cos(phi), -sin(phi)*sin(s*k), -(1/k)*(sin(phi)*cos(s*k)); sin(s*k), 0, cos(s*k), (1/k)*sin(s*k);0, 0, 0, 1];
+posV = [M2(1,4) ;M2(2,4); M2(3,4)]
+
+% solve system of equations to find phi and k
+equ1 = posV(1) == x
+equ2 = posV(2) == y 
+equ3 =posV(3) == z
+
+sol = solve([equ1, equ2, equ3], [phi k]);
+phiSol = sol.phi;
+kSol = sol.k;
+
+end
